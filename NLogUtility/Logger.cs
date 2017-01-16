e7dc932a9bc9cf145a706c86db16130643ccfa73
kinshines <1176807665@qq.com>
2017-01-16 16:01:34 +0800

    initial

---
+++ NLogUtility/Logger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NLogUtility
+{
+    public class Logger
+    {
+        public static void Info(string module, string message, params object[] args)
+        {
+            NLog.Logger logger = NLog.LogManager.GetLogger(module);
+            logger.Info(message, args);
+        }
+
+        public static void Trace(string module, string message, params object[] args)
+        {
+            NLog.Logger logger = NLog.LogManager.GetLogger(module);
+            logger.Trace(message, args);
+        }
+
+        public static void Error(Exception ex, string message, params object[] args)
+        {
+            NLog.Logger logger = NLog.LogManager.GetLogger("error");
+            logger.Error(ex, message, args);
+        }
+        public static void Error(Exception ex)
+        {
+            NLog.Logger logger = NLog.LogManager.GetLogger("error");
+            logger.Error(ex);
+        }
+    }
+}
