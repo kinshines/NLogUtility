886d69f361ffe0b74708e6ecc6e51dac78cb04bf
kinshines <1176807665@qq.com>
2017-01-19 15:35:22 +0800

    nuget

--- NLogUtility/Properties/AssemblyInfo.cs
+++ NLogUtility/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过以下
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("NLogUtility")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("NLogUtility for .NET Framework 4.5")]
+[assembly: AssemblyDescription("a utility for nlog")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("HaiMaWan")]
 [assembly: AssemblyProduct("NLogUtility")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyCopyright("Copyright © HaiMaWan 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // 可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.5")]
+[assembly: AssemblyFileVersion("1.0.5")]
